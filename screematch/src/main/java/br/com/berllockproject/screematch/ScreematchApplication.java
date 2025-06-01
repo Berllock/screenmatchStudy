ddecc01a84bded4a31b47683462cc9a9c03754e6
berllocK <brenosouzaemp@gmail.com>
2025-06-01 00:23:37 -0300

    start project

---
+++ screematch/src/main/java/br/com/berllockproject/screematch/ScreematchApplication.java
@@ -0,0 +1,26 @@
+package br.com.berllockproject.screematch;
+
+import br.com.berllockproject.screematch.model.DadosSerie;
+import br.com.berllockproject.screematch.service.ConsumoApi;
+import br.com.berllockproject.screematch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreematchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreematchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=a829c87d");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
